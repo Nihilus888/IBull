151d1b3df46064708491b751847e114d9ab371eb
Elliott Tay <austonmartin@gmail.com>
2022-10-14 18:23:43 +0800

    added title in home page

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,7 @@ import './App.css'
 import { Routes, Route } from 'react-router-dom'
 import React, { useEffect } from 'react'
 import Home from './components/home/Home'
-import SiteHeader from './components/partials/SiteHeader'
-import SiteFooter from './components/partials/SiteFooter'
+import StickyFooter from './components/partials/SiteFooter'
 
 function App() {
   // const profile = (<SiteHeader id={} showViewButton={true} />)
@@ -11,7 +10,8 @@ function App() {
   return (
   
     <div className="App">
-      <SiteFooter />
+      <Home />
+      <StickyFooter />
     </div>
   );
 }
