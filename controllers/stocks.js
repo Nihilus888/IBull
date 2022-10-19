e493c5300a5c6f3ac8f66c4abf6c57ae466b835a
Elliott Tay <austonmartin@gmail.com>
2022-10-19 19:40:43 +0800

    cleanup for list stock controller

--- controllers/stocks.js
+++ controllers/stocks.js
@@ -12,28 +12,12 @@ module.exports = {
         console.log('searchStr:', searchStr)
 
         const chart = await fetch(`https://query1.finance.yahoo.com/v8/finance/chart/${searchStr}`)
-            // .then(response => {
-            //     const isJson = response.headers.get('content-type')?.includes('application/json');
-            //     const chartData = isJson ? response.json(): null
-
-            //     if(!response.ok) {
-            //         const error = (chartData)
-            //         return Promise.reject(error)
-            //     }
-            // })
-            // .catch(error => {
-            //     console.log('There was an error!', error)
-            // })
 
         const chartData = await chart.json()
         
             //stock info
         const stockInfo = await fetch(`https://query1.finance.yahoo.com/v10/finance/quoteSummary/${searchStr}?modules=defaultKeyStatistics`)
         
-        //get data from the API response resolve response
-        // const chartData = await chart.json()
-        //console.log(chartData.chart.result)
-
         //closeData price for this particular stock
         closeData = chartData.chart.result[0].indicators.quote[0].close
         console.log('closeData: ', closeData)
@@ -43,24 +27,12 @@ module.exports = {
             })
 
         console.log(result)
-        
-
-        // console.log('stockPrice: ', stockPrice)
-        // console.log(typeof(stockPrice))
-        
-        
-        //loop through the array and push it into result
-        // console.log('result', result)
-        // console.log(typeof(result))
-
 
         //get the financial ratios to display in the frontend for later
         const stockData = await stockInfo.json()
-        //console.log(stockData.quoteSummary.result[0].defaultKeyStatistics)
 
         // //get the individual details from the data like name and currency
         let name = chartData.chart.result[0].meta.symbol
-        //console.log('name:', name)
         let currency = chartData.chart.result[0].meta.currency
 
         // //get the individual financial ratio details
@@ -77,8 +49,6 @@ module.exports = {
         //push all the individuals to stock
         let stock = []
         stock.push(enterpriseValue, forwardPE, profitMargins, floatShares, sharesOutstanding, sharesShort, shortRatio, beta, priceToBook)
-        // console.log('stock', stock)
-        // console.log(typeof(stock))
 
         let financialInfo = result.push(stock)
         let finance = JSON.stringify(financialInfo)
