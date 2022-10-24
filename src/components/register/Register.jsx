753ba44c2243a554bbd81f95707a7210f96eabdb
Elliott Tay <austonmartin@gmail.com>
2022-10-24 18:59:07 +0800

    added logo to registration page

--- src/components/register/Register.jsx
+++ src/components/register/Register.jsx
@@ -1,5 +1,5 @@
 import * as React from "react";
-import {useState} from 'react';
+import { useState } from "react";
 import Avatar from "@mui/material/Avatar";
 import Button from "@mui/material/Button";
 import CssBaseline from "@mui/material/CssBaseline";
@@ -16,13 +16,13 @@ import { createTheme, ThemeProvider } from "@mui/material/styles";
 import Slider from "@mui/material/Slider";
 import Stack from "@mui/material/Stack";
 import Autocomplete from "@mui/material/Autocomplete";
-import Image from '../../components/stockmarket.jpg'
+import Image from "../../components/stockmarket.jpg";
 import { useNavigate } from "react-router-dom";
-
-
+import FacebookIcon from '@mui/icons-material/Facebook';
+import GoogleIcon from '@mui/icons-material/Google';
+import TwitterIcon from '@mui/icons-material/Twitter';
 
 const theme = createTheme();
-
 
 export default function Register() {
   const navigate = useNavigate();
@@ -32,7 +32,7 @@ export default function Register() {
   const [details, setDetails] = useState("");
   const [titleError, setTitleError] = useState("");
   const [detailsError, setDetailsError] = useState("");
-  const [financeSkill, setFinanceSkill] = useState([])
+  const [financeSkill, setFinanceSkill] = useState([]);
   const [formData, setFormData] = useState({
     name: "",
     email: "",
@@ -48,14 +48,14 @@ export default function Register() {
     setFormData({
       ...formData,
       skills: [...financeSkill],
-      [e.target.name]: e.target.value
+      [e.target.name]: e.target.value,
     });
   };
 
   const handleChangeOnSubmit = () => {
     setFormData({
       ...formData,
-      skills: [...financeSkill]
+      skills: [...financeSkill],
     });
   };
 
@@ -63,22 +63,21 @@ export default function Register() {
     event.preventDefault();
 
     //initialize when details and error is not false
-    setTitleError(false)
-    setDetailsError(false)
+    setTitleError(false);
+    setDetailsError(false);
 
-    if(title === '') {
-      setTitleError(true)
+    if (title === "") {
+      setTitleError(true);
     }
 
-    if(details === '') {
-      setDetailsError(true)
+    if (details === "") {
+      setDetailsError(true);
     }
-
 
     if (title && details) {
       console.log(title, details);
     }
-    
+
     console.log({
       email: formData.email,
       password: formData.password,
@@ -86,240 +85,245 @@ export default function Register() {
       job: formData.job,
       position: formData.position,
       skills: formData.skills,
-      networth: formData.networth
+      networth: formData.networth,
     });
-      // Need to add the mongodb here?
-      fetch("http://localhost:3001/user/register", {
-        method: 'POST',
-        body: JSON.stringify(formData),
-        headers: {
-            'Content-type': 'application/json',
-        },
+    // Need to add the mongodb here?
+    fetch("http://localhost:3001/user/register", {
+      method: "POST",
+      body: JSON.stringify(formData),
+      headers: {
+        "Content-type": "application/json",
+      },
     })
-        .then(response => {
-            console.log('response: ',response)
-            return response.json()
-        })
-        .then(jsonResponse => {
-            if (jsonResponse.error) {
-                console.log('jsonResponse.error: ', jsonResponse.error)
-                return
-            }
+      .then((response) => {
+        console.log("response: ", response);
+        return response.json();
+      })
+      .then((jsonResponse) => {
+        if (jsonResponse.error) {
+          console.log("jsonResponse.error: ", jsonResponse.error);
+          return;
+        }
 
-            console.log('Registration Successful!')
-            
-            //remember to deal navigate straight to home logged in page
-            navigate('/')
-        })
-        .catch(err => {
-            console.log('err: ',err)
-        })
+        console.log("Registration Successful!");
+
+        //remember to deal navigate straight to home logged in page
+        navigate("/");
+      })
+      .catch((err) => {
+        console.log("err: ", err);
+      });
   };
 
   return (
     <ThemeProvider theme={theme}>
-      <Box sx={{ 
-            backdropFilter: "blur(3px)",
-            backgroundRepeat: 'no-repeat',
-            backgroundSize: 'Cover',
-            backgroundImage: `url(${Image})`,
-            bgcolor: 'white',
-          }}>
-      <Container component="main" maxWidth="xs">
-        <CssBaseline />
-        <Box
-          sx={{
-            pt: 4,
-            display: "flex",
-            flexDirection: "column",
-            alignItems: "center",
-          }}
-        >
-          <Avatar sx={{ m: 1, bgcolor: "secondary.main" }}>
-            <LockOutlinedIcon />
-          </Avatar>
-          <Typography component="h1" variant="h5" fontWeight='bold'>
-            Sign up
-          </Typography>
+      <Box
+        sx={{
+          backdropFilter: "blur(3px)",
+          backgroundRepeat: "no-repeat",
+          backgroundSize: "Cover",
+          backgroundImage: `url(${Image})`,
+          bgcolor: "white",
+        }}
+      >
+        <Container component="main" maxWidth="xs">
+          <CssBaseline />
           <Box
-            component="form"
-            noValidate
-            onSubmit={handleSubmit}
-            sx={{ mt: 3 }}
+            sx={{
+              pt: 4,
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "center",
+            }}
           >
-            <Grid container spacing={2}>
-              <Grid item xs={12}>
-                <TextField
-                  autoComplete="given-name"
-                  name="name"
-                  required
-                  fullWidth
-                  id="name"
-                  label="Name"
-                  autoFocus
-                  value={formData.name}
-                  onChange={handleChange}
-                  error={titleError}
-                />
-              </Grid>
-
-              <Grid item xs={12}>
-                <TextField
-                  required
-                  fullWidth
-                  id="email"
-                  label="Email Address"
-                  name="email"
-                  autoComplete="email"
-                  value={formData.email}
-                  onChange={handleChange}
-                  error={titleError}
-                />
-              </Grid>
-              <Grid item xs={12}>
-                <TextField
-                  required
-                  fullWidth
-                  name="password"
-                  label="Password"
-                  type="password"
-                  id="password"
-                  autoComplete="new-password"
-                  value={formData.password}
-                  onChange={handleChange}
-                  error={titleError}
-                />
-              </Grid>
-
-              <Grid item xs={12}>
-                <TextField
-                  required
-                  fullWidth
-                  name="confirmPassword"
-                  label="Confirm Password"
-                  type="Password"
-                  id="confirmPassword"
-                  autoComplete="new-password"
-                  value={formData.confirmPassword}
-                  onChange={handleChange}
-                  error={titleError}
-                />
-              </Grid>
-
-              <Grid item xs={12} sm={6}>
-                <TextField
-                  required
-                  fullWidth
-                  id="job"
-                  label="Job Title"
-                  name="job"
-                  autoComplete="job"
-                  value={formData.job}
-                  onChange={handleChange}
-                  error={detailsError}
-                />
-              </Grid>
-
-              <Grid item xs={12} sm={6}>
-                <TextField
-                  required
-                  fullWidth
-                  id="position"
-                  label="Position"
-                  name="position"
-                  autoComplete="position"
-                  value={formData.position}
-                  onChange={handleChange}
-                  error={detailsError}
-                />
-              </Grid>
-
-              <Grid item xs={12}>
-                <Typography id="input-slider" gutterBottom>
-                  Experience*
-                </Typography>
-                <Slider
-                  defaultValue={0}
-                  aria-label="Default"
-                  valueLabelDisplay="auto"
-                  max={50}
-                  id="experience"
-                  name="experience"
-                  label="experience"
-                  value={formData.experience}
-                  onChange={handleChange}
-                  error={detailsError}
-                />
-              </Grid>
-
-              <Grid item xs={12}>
-                <Stack spacing={3} sx={{ width: 500 }}>
-                  <Autocomplete
-                    multiple
-                    name="skills"
-                    id="skills"
-                    onChange={(event,value) => setFinanceSkill(value)}
-                    value={financeSkill}
-                    autoSelect
-                    options={setskills}
-                    getOptionLabel={(option) => option.name}
-                    defaultValue={setskills[0]}
-                    error={detailsError}
-                    renderInput={(params) => (
-                    <TextField
-                      {...params}
-                      variant="standard"
-                      label="Add Your Finance Skills"
-                      placeholder="Skills"
-                    />
-                  )}
-                />
-                </Stack>
-              </Grid>
-
-              <Grid item xs={12}>
-                <TextField
-                  required
-                  fullWidth
-                  name="networth"
-                  label="Networth"
-                  type="networth"
-                  id="networth"
-                  autoComplete="networth"
-                  value={formData.networth}
-                  onChange={handleChange}
-                  error={titleError}
-                />
-              </Grid>
-
-              <Grid item xs={12}>
-                <FormControlLabel
-                  control={
-                    <Checkbox value="allowExtraEmails" color="primary" />
-                  }
-                  label="I want to receive alerts on new jobs via email."
-                />
-              </Grid>
-            </Grid>
-            <Button
-              type="submit"
-              onClick={handleChangeOnSubmit}
-              fullWidth
-              variant="contained"
-              sx={{ mt: 3, mb: 2 }}
+            <Avatar sx={{ m: 1, bgcolor: "secondary.main" }}>
+              <LockOutlinedIcon />
+            </Avatar>
+            <Typography component="h1" variant="h5" fontWeight="bold">
+              Sign up
+            </Typography>
+            <Box
+              component="form"
+              noValidate
+              onSubmit={handleSubmit}
+              sx={{ mt: 3 }}
             >
-              Sign Up
-            </Button>
-            <Grid container justifyContent="center" mb='40px'>
-              <Grid item>
-                <Link href="/login" variant="body2" fontWeight='bold'>
-                  Already have an account? Sign in
-                </Link>
+              <Grid container spacing={2}>
+                <Grid item xs={12}>
+                  <TextField
+                    autoComplete="given-name"
+                    name="name"
+                    required
+                    fullWidth
+                    id="name"
+                    label="Name"
+                    autoFocus
+                    value={formData.name}
+                    onChange={handleChange}
+                    error={titleError}
+                  />
+                </Grid>
+
+                <Grid item xs={12}>
+                  <TextField
+                    required
+                    fullWidth
+                    id="email"
+                    label="Email Address"
+                    name="email"
+                    autoComplete="email"
+                    value={formData.email}
+                    onChange={handleChange}
+                    error={titleError}
+                  />
+                </Grid>
+                <Grid item xs={12}>
+                  <TextField
+                    required
+                    fullWidth
+                    name="password"
+                    label="Password"
+                    type="password"
+                    id="password"
+                    autoComplete="new-password"
+                    value={formData.password}
+                    onChange={handleChange}
+                    error={titleError}
+                  />
+                </Grid>
+
+                <Grid item xs={12}>
+                  <TextField
+                    required
+                    fullWidth
+                    name="confirmPassword"
+                    label="Confirm Password"
+                    type="Password"
+                    id="confirmPassword"
+                    autoComplete="new-password"
+                    value={formData.confirmPassword}
+                    onChange={handleChange}
+                    error={titleError}
+                  />
+                </Grid>
+
+                <Grid item xs={12} sm={6}>
+                  <TextField
+                    required
+                    fullWidth
+                    id="job"
+                    label="Job Title"
+                    name="job"
+                    autoComplete="job"
+                    value={formData.job}
+                    onChange={handleChange}
+                    error={detailsError}
+                  />
+                </Grid>
+
+                <Grid item xs={12} sm={6}>
+                  <TextField
+                    required
+                    fullWidth
+                    id="position"
+                    label="Position"
+                    name="position"
+                    autoComplete="position"
+                    value={formData.position}
+                    onChange={handleChange}
+                    error={detailsError}
+                  />
+                </Grid>
+
+                <Grid item xs={12}>
+                  <Typography id="input-slider" gutterBottom>
+                    Experience*
+                  </Typography>
+                  <Slider
+                    defaultValue={0}
+                    aria-label="Default"
+                    valueLabelDisplay="auto"
+                    max={50}
+                    id="experience"
+                    name="experience"
+                    label="experience"
+                    value={formData.experience}
+                    onChange={handleChange}
+                    error={detailsError}
+                  />
+                </Grid>
+
+                <Grid item xs={12}>
+                  <Stack spacing={3} sx={{ width: 500 }}>
+                    <Autocomplete
+                      multiple
+                      name="skills"
+                      id="skills"
+                      onChange={(event, value) => setFinanceSkill(value)}
+                      value={financeSkill}
+                      autoSelect
+                      options={setskills}
+                      getOptionLabel={(option) => option.name}
+                      defaultValue={setskills[0]}
+                      error={detailsError}
+                      renderInput={(params) => (
+                        <TextField
+                          {...params}
+                          variant="standard"
+                          label="Add Your Finance Skills"
+                          placeholder="Skills"
+                        />
+                      )}
+                    />
+                  </Stack>
+                </Grid>
+
+                <Grid item xs={12}>
+                  <TextField
+                    required
+                    fullWidth
+                    name="networth"
+                    label="Networth"
+                    type="networth"
+                    id="networth"
+                    autoComplete="networth"
+                    value={formData.networth}
+                    onChange={handleChange}
+                    error={titleError}
+                  />
+                </Grid>
+
+                <Grid item xs={12}>
+                  <FormControlLabel
+                    control={
+                      <Checkbox value="allowExtraEmails" color="primary" />
+                    }
+                    label="I want to receive alerts on new jobs via email."
+                  />
+                </Grid>
               </Grid>
-            </Grid>
+              <Button
+                type="submit"
+                onClick={handleChangeOnSubmit}
+                fullWidth
+                variant="contained"
+                sx={{ mt: 3, mb: 2 }}
+              >
+                Sign Up
+              </Button>
+              <Grid container justifyContent="center" mb="40px">
+                <Grid item>
+                  <Link href="/login" variant="body2" fontWeight="bold">
+                    Already have an account? Sign in
+                  </Link>
+                </Grid>
+              </Grid>
+            </Box>
           </Box>
-        </Box>
-      </Container>
+          <FacebookIcon sx={{ ml: 3, mb: 5 }} />
+          <GoogleIcon sx={{ ml: 3, mb: 5 }} />
+          <TwitterIcon sx={{ ml: 3, mb: 5 }} />
+        </Container>
       </Box>
     </ThemeProvider>
   );
