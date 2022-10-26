ff67eaffaaec42f7e208b8cb2c0a95cc53f8e4f0
Elliott Tay <austonmartin@gmail.com>
2022-10-26 16:43:33 +0800

    style chart

--- src/components/stockView/Line.jsx
+++ src/components/stockView/Line.jsx
@@ -6,95 +6,51 @@ import { Line } from "react-chartjs-2";
 ChartJS.register(LineElement);
 
 function LineChart(props) {
- const lineData = {
-    labels: ["Price", "Date"],
+  const lineData = {
+    labels: ["Price", "Hour"],
     datasets: [
       {
-        label: "# of Votes",
+        label: "Stock price over the trading day",
         data: props.data,
-        backgroundColor: [
-          "rgba(255, 99, 132, 0.2)",
-          "rgba(54, 162, 235, 0.2)",
-          "rgba(255, 206, 86, 0.2)",
-          "rgba(75, 192, 192, 0.2)",
-          "rgba(153, 102, 255, 0.2)",
-          "rgba(255, 159, 64, 0.2)",
-        ],
-        borderColor: [
-          "rgba(255, 99, 132, 1)",
-          "rgba(54, 162, 235, 1)",
-          "rgba(255, 206, 86, 1)",
-          "rgba(75, 192, 192, 1)",
-          "rgba(153, 102, 255, 1)",
-          "rgba(255, 159, 64, 1)",
-        ],
-        borderWidth: 1,
+        backgroundColor: ["rgb(75, 192, 192)"],
+        borderColor: ["rgba(255, 99, 132, 1)"],
+        fill: false,
+        tension: 0.1,
+        borderDash: [],
+        borderDashOffset: 0.0,
+        borderJoinStyle: "miter",
+        borderCapStyle: "butt",
+        pointBorderColor: "rgba(75,192,192,1)",
+        pointBorderWidth: 5,
+        pointHoverRadius: 10,
+        pointRadius: 5,
+        pointHitRadius: 20,
       },
     ],
   };
 
-  let delayed
-
-  // useEffect(() => {
-  //   const fetchStockData = async () => {
-  //     await fetch("http://localhost:3001/stock/search", {
-  //       method: 'GET',
-  //       headers: {
-  //         'Content-Type': 'application/json',
-  //       }
-  //     })
-  //       .then((response) => {
-  //         if (response.ok) {
-  //           response.json().then((json) => {
-  //             const c = {...chart}
-  //             c.data = json.data
-  //             setChart(c)
-  //           });
-  //         }
-  //       }).catch((error) => {
-  //         console.log(error);
-  //       });
-  //   };
-  //   fetchStockData()
-  // }, [])
-
-  // let data = {
-  //   labels: ["Price", "Date"],
-  //   datasets: [
-  //     {
-  //       label: "# of Votes",
-  //       data: [12, 19, 3, 5, 2, 3],
-  //       backgroundColor: [
-  //         "rgba(255, 99, 132, 0.2)",
-  //         "rgba(54, 162, 235, 0.2)",
-  //         "rgba(255, 206, 86, 0.2)",
-  //         "rgba(75, 192, 192, 0.2)",
-  //         "rgba(153, 102, 255, 0.2)",
-  //         "rgba(255, 159, 64, 0.2)",
-  //       ],
-  //       borderColor: [
-  //         "rgba(255, 99, 132, 1)",
-  //         "rgba(54, 162, 235, 1)",
-  //         "rgba(255, 206, 86, 1)",
-  //         "rgba(75, 192, 192, 1)",
-  //         "rgba(153, 102, 255, 1)",
-  //         "rgba(255, 159, 64, 1)",
-  //       ],
-  //       borderWidth: 1,
-  //     },
-  //   ],
-  // };
+  let delayed;
 
   let options = {
     maintainAspectRatio: false,
     scales: {
       y: {
+        beginAtZero: false,
+        stacked: false,
+      },
+      x: {
         beginAtZero: true,
+        stacked: true,
+        labels: [
+          1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
+          21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35
+        ],
       },
     },
     legend: {
       labels: {
-        fontSize: 26,
+        fontSize: 30,
+        fontWeight: 'bold',
       },
     },
 
@@ -104,7 +60,7 @@ function LineChart(props) {
       },
       delay: (context) => {
         let delay = 5;
-        if (context.type === 'data' && context.mode === 'default' && !delayed) {
+        if (context.type === "data" && context.mode === "default" && !delayed) {
           delay = context.dataIndex * 600 + context.datasetIndex * 300;
         }
         return delay;
